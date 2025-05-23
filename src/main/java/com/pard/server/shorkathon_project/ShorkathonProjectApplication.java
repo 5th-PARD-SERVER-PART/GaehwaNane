d8491a0d9dd0a1042fea30e5898a351b532319dc
eaststar113 <95015653+eaststar113@users.noreply.github.com>
2025-05-23 19:07:04 +0900

    [back] feat : Initial project

---
+++ src/main/java/com/pard/server/shorkathon_project/ShorkathonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.shorkathon_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorkathonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorkathonProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/server/shorkathon_project/ShorkathonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.shorkathon_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorkathonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
